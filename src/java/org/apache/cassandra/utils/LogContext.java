6b87f706369f30fe4617a5d1b40b7d1faba168e8
J.P.Eiti Kimura <eiti.kimura@movile.com>
2015-11-24 14:51:46 -0800

    Add LogContext class to handle MDC for column family names and keyspace name
    This class uses MDC to set at thread context the map and print in logs the
    name of CF and KS of certain type of operations as described on CASSANDRA-7276

---
+++ src/java/org/apache/cassandra/utils/LogContext.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cassandra.utils;
+
+import org.slf4j.MDC;
+
+public final class LogContext
+{
+
+    private static final String COLUMN_FAMILY = "cf";
+    private static final String KEYSPACE = "ks";
+
+    public static final void setKeyspace(String ks)
+    {
+        if (ks != null)
+            MDC.put(KEYSPACE, String.format("|%s", ks));
+    }
+
+    public static final void setColumnFamily(String cf)
+    {
+        if (cf != null)
+            MDC.put(COLUMN_FAMILY, String.format(".%s", cf));
+    }
+
+    public static final void setKeyspaceAndColumnFamily(String ks, String cf)
+    {
+        setKeyspace(ks);
+        setColumnFamily(cf);
+    }
+
+    public static final void clear()
+    {
+        MDC.clear();
+    }
+}
