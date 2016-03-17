8dccb6c6d7caeb67d376dc296a0d022a77da5348
Paulo Motta <pauloricardomg@gmail.com>
2016-03-17 20:16:04 -0300

    Fix nodetool join does not transition from write survey to normal mode
    patch by Paulo Motta; reviewed by Yuki Morishita for CASSANDRA-11379

--- src/java/org/apache/cassandra/service/StorageServiceMBean.java
+++ src/java/org/apache/cassandra/service/StorageServiceMBean.java
@@ -481,6 +481,7 @@ public interface StorageServiceMBean extends NotificationEmitter
     // allows a node that have been started without joining the ring to join it
     public void joinRing() throws IOException;
     public boolean isJoined();
+    public boolean isSurveyMode();
 
     public void setStreamThroughputMbPerSec(int value);
     public int getStreamThroughputMbPerSec();
