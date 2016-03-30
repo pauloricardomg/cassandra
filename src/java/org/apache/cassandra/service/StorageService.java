873c9984e8bf3c979b1b4bf6e352767d26e7e0ff
Paulo Motta <pauloricardomg@gmail.com>
2016-03-30 16:07:42 -0300

    Fix nodetool join does not transition from write survey to normal mode
    patch by Paulo Motta; reviewed by Yuki Morishita for CASSANDRA-11379

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -1124,7 +1124,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
 
     public boolean isJoined()
     {
-        return tokenMetadata.isMember(FBUtilities.getBroadcastAddress());
+        return tokenMetadata.isMember(FBUtilities.getBroadcastAddress()) && !isSurveyMode;
     }
 
     public void rebuild(String sourceDc)
