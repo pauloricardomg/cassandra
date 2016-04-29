e8acb0bd717bdc03d4ba04d4935e60341f6e4e11
Paulo Motta <pauloricardomg@gmail.com>
2016-04-29 19:56:45 -0300

    Increase incoming socket timeout on sender during 2i/MV rebuild on receiver
    Patch by Paulo Motta; reviewed by Yuki Morishita for CASSANDRA-8343

--- src/java/org/apache/cassandra/streaming/StreamSession.java
+++ src/java/org/apache/cassandra/streaming/StreamSession.java
@@ -20,6 +20,7 @@ package org.apache.cassandra.streaming;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -115,6 +116,7 @@ import org.apache.cassandra.utils.concurrent.Refs;
 public class StreamSession implements IEndpointStateChangeSubscriber
 {
     private static final Logger logger = LoggerFactory.getLogger(StreamSession.class);
+    private static final Integer SOCKET_TIMEOUT_MULTIPLIER = 3;
 
     /**
      * Streaming endpoint.
@@ -141,7 +143,7 @@ public class StreamSession implements IEndpointStateChangeSubscriber
 
     public final ConnectionHandler handler;
 
-    private int retries;
+    private int retries = 0;
 
     private AtomicBoolean isAborted = new AtomicBoolean(false);
     private final boolean keepSSTableLevel;
@@ -516,10 +518,16 @@ public class StreamSession implements IEndpointStateChangeSubscriber
      */
     public void onError(Throwable e)
     {
-        logger.error("[Stream #{}] Streaming error occurred on session with peer {}{}", planId(),
-                                                                                        peer.getHostAddress(),
-                                                                                        peer.equals(connecting) ? "" : " through " + connecting.getHostAddress(),
-                                                                                        e);
+        if (state() == State.WAIT_COMPLETE && e instanceof SocketTimeoutException)
+            logger.error("[Stream #{}] Received stream socket timeout on WAIT_COMPLETE state and will fail stream session. " +
+                         "The other peer might still be processing received data. If that's the case, consider increasing " +
+                         "streaming_socket_timeout_in_ms property to allow more time for the other peer to process received data.",
+                         planId());
+        else
+            logger.error("[Stream #{}] Streaming error occurred on session with peer {}{}", planId(),
+                                                                                            peer.getHostAddress(),
+                                                                                            peer.equals(connecting) ? "" : " through " + connecting.getHostAddress(),
+                                                                                            e);
         // send session failure message
         if (handler.isOutgoingConnected())
             handler.sendMessage(new SessionFailedMessage());
@@ -715,6 +723,11 @@ public class StreamSession implements IEndpointStateChangeSubscriber
                 completeSent = true;
                 state(State.WAIT_COMPLETE);
                 handler.closeOutgoing();
+                //Increase incoming socket timeout to allow more time for receiver
+                //to process data without failing stream session (CASSANDRA-8343)
+                handler.setIncomingSocketTimeout((int)Math.min((long) SOCKET_TIMEOUT_MULTIPLIER *
+                                                               DatabaseDescriptor.getStreamingSocketTimeout(),
+                                                               Integer.MAX_VALUE));
             }
         }
         return completed;
