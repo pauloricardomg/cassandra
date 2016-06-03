476bba5139f7d77a8b025c09bea014e80bd90524
Paulo Motta <pauloricardomg@gmail.com>
2016-06-03 12:17:57 -0300

    Remove finished incoming streaming connections from MessagingService
    patch by Paulo Motta; reviewed by Yuki Morishita for CASSANDRA-11854

--- src/java/org/apache/cassandra/net/IncomingStreamingConnection.java
+++ src/java/org/apache/cassandra/net/IncomingStreamingConnection.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.net.Socket;
 import java.util.Set;
 
+import com.google.common.util.concurrent.MoreExecutors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -72,13 +73,24 @@ public class IncomingStreamingConnection extends Thread implements Closeable
             // The receiving side distinguish two connections by looking at StreamInitMessage#isForOutgoing.
             // Note: we cannot use the same socket for incoming and outgoing streams because we want to
             // parallelize said streams and the socket is blocking, so we might deadlock.
-            StreamResultFuture.initReceivingSide(init.sessionIndex, init.planId, init.description, init.from, socket, init.isForOutgoing, version, init.keepSSTableLevel, init.isIncremental);
+            StreamResultFuture future = StreamResultFuture.initReceivingSide(init.sessionIndex, init.planId, init.description, init.from, socket, init.isForOutgoing, version, init.keepSSTableLevel, init.isIncremental);
+
+            //register callback only once per StreamResultFuture
+            if (init.isForOutgoing)
+            {
+                future.addListener(new Runnable()
+                {
+                    public void run()
+                    {
+                        group.remove(IncomingStreamingConnection.this);
+                    }
+                }, MoreExecutors.sameThreadExecutor());
+            }
         }
         catch (IOException e)
         {
             logger.error(String.format("IOException while reading from socket from %s, closing: %s",
                                        socket.getRemoteSocketAddress(), e));
-            logger.trace(String.format("IOException while reading from socket from %s, closing", socket.getRemoteSocketAddress()), e);
             close();
         }
     }
