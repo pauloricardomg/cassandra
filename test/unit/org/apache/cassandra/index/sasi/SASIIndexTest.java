6f6b246603eb177f033fc011bc271ba893015e88
Paulo Motta <paulo@apache.org>
2022-02-10 19:26:39 -0300

    Fix SASIIndexTest.testSASIComponentsAddedToSnapshot

--- test/unit/org/apache/cassandra/index/sasi/SASIIndexTest.java
+++ test/unit/org/apache/cassandra/index/sasi/SASIIndexTest.java
@@ -42,6 +42,7 @@ import org.apache.cassandra.cql3.CQLTester;
 import org.apache.cassandra.cql3.Operator;
 import org.apache.cassandra.cql3.QueryProcessor;
 import org.apache.cassandra.cql3.UntypedResultSet;
+import org.apache.cassandra.db.lifecycle.LifecycleTransaction;
 import org.apache.cassandra.index.Index;
 import org.apache.cassandra.schema.ColumnMetadata;
 import org.apache.cassandra.schema.Schema;
@@ -151,7 +152,6 @@ public class SASIIndexTest
             data.put(UUID.randomUUID().toString(), Pair.create(UUID.randomUUID().toString(), r.nextInt()));
 
         ColumnFamilyStore store = loadData(data, true);
-        store.forceMajorCompaction();
 
         Set<SSTableReader> ssTableReaders = store.getLiveSSTables();
         Set<Component> sasiComponents = new HashSet<>();
@@ -165,6 +165,10 @@ public class SASIIndexTest
         try
         {
             store.snapshot(snapshotName);
+            // Compact to make true snapshot size != 0
+            store.forceMajorCompaction();
+            LifecycleTransaction.waitForDeletions();
+
             FileReader reader = new FileReader(store.getDirectories().getSnapshotManifestFile(snapshotName));
             JSONObject manifest = (JSONObject) new JSONParser().parse(reader);
             JSONArray files = (JSONArray) manifest.get("files");
@@ -197,8 +201,7 @@ public class SASIIndexTest
 
                 for (Component c : components)
                 {
-                    Path componentPath = Paths.get(sstable.descriptor + "-" + c.name);
-                    long componentSize = Files.size(componentPath);
+                    long componentSize = Files.size(Paths.get(snapshotSSTable.filenameFor(c)));
                     if (Component.Type.fromRepresentation(c.name) == Component.Type.SECONDARY_INDEX)
                         indexSize += componentSize;
                     else
@@ -209,7 +212,7 @@ public class SASIIndexTest
             Map<String, Directories.SnapshotSizeDetails> details = store.getSnapshotDetails();
 
             // check that SASI components are included in the computation of snapshot size
-            Assert.assertEquals((long) details.get(snapshotName).dataSizeBytes, tableSize + indexSize);
+            Assert.assertEquals(tableSize + indexSize, (long) details.get(snapshotName).dataSizeBytes);
         }
         finally
         {
